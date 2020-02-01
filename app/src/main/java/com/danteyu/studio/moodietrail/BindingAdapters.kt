cf970b7ecfb9239aa5f545b1ebe92cf0475da883
aptx113 <aptx113@gmail.com>
2020-02-01 19:16:10 +0800

    setup first version of layout for NoteFragment and item

--- app/src/main/java/com/danteyu/studio/moodietrail/BindingAdapters.kt
+++ app/src/main/java/com/danteyu/studio/moodietrail/BindingAdapters.kt
@@ -1,2 +1,80 @@
 package com.danteyu.studio.moodietrail
 
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import com.danteyu.studio.moodietrail.network.LoadApiStatus
+
+
+
+@BindingAdapter("itemPosition", "itemCount")
+fun setupPaddingForGridItems(layout: ConstraintLayout, position: Int, count: Int) {
+
+    val outsideHorizontal = MoodieTrailApplication.instance.resources.getDimensionPixelSize(R.dimen.space_outside_horizontal_note_item)
+    val insideHorizontal = MoodieTrailApplication.instance.resources.getDimensionPixelSize(R.dimen.space_inside_horizontal_note_item)
+    val outsideVertical = MoodieTrailApplication.instance.resources.getDimensionPixelSize(R.dimen.space_outside_vertical_note_item)
+    val insideVertical = MoodieTrailApplication.instance.resources.getDimensionPixelSize(R.dimen.space_inside_vertical_note_item)
+
+    val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+
+    when {
+        position == 0 -> { // first item and confirm whether only 1 row
+            layoutParams.setMargins(outsideHorizontal, outsideVertical, insideHorizontal, if (count > 2) insideVertical else outsideVertical)
+        }
+        position == 1 -> { // second item and confirm whether only 1 row
+            layoutParams.setMargins(insideHorizontal, outsideVertical, outsideHorizontal, if (count > 2) insideVertical else outsideVertical)
+        }
+        count % 2 == 0 && position == count - 1 -> { // count more than 2 and item count is even
+            layoutParams.setMargins(insideHorizontal, insideVertical, outsideHorizontal, outsideVertical)
+        }
+        (count % 2 == 1 && position == count - 1) || (count % 2 == 0 && position == count - 2) -> {
+            layoutParams.setMargins(outsideHorizontal, insideVertical, insideHorizontal, outsideVertical)
+        }
+        position % 2 == 0 -> { // even
+            when (position) {
+                count - 1 -> layoutParams.setMargins(insideHorizontal, insideVertical, outsideHorizontal, outsideVertical) // last 1
+                count - 2 -> layoutParams.setMargins(outsideHorizontal, insideVertical, insideHorizontal, outsideVertical) // last 2
+                else -> layoutParams.setMargins(outsideHorizontal, insideVertical, insideHorizontal, insideVertical)
+            }
+        }
+        position % 2 == 1 -> { // odd
+            when (position) {
+                count - 1 -> layoutParams.setMargins(outsideHorizontal, insideVertical, insideHorizontal, outsideVertical) // last 1
+                else -> layoutParams.setMargins(insideHorizontal, insideVertical, outsideHorizontal, insideVertical)
+            }
+        }
+    }
+
+    layout.layoutParams = layoutParams
+}
+
+//General
+
+/**
+ * According to [LoadApiStatus] to decide the visibility of [ProgressBar]
+ */
+@BindingAdapter("setupApiStatus")
+fun bindApiStatus(view: ProgressBar, status: LoadApiStatus?) {
+    when (status) {
+        LoadApiStatus.LOADING -> view.visibility = View.VISIBLE
+        LoadApiStatus.DONE, LoadApiStatus.ERROR -> view.visibility = View.GONE
+    }
+}
+
+/**
+ * According to [message] to decide the visibility of [ProgressBar]
+ */
+@BindingAdapter("setupApiErrorMessage")
+fun bindApiErrorMessage(view: TextView, message: String?) {
+    when (message) {
+        null, "" -> {
+            view.visibility = View.GONE
+        }
+        else -> {
+            view.text = message
+            view.visibility = View.VISIBLE
+        }
+    }
+}
--- app/src/main/java/com/danteyu/studio/moodietrail/note/NoteViewModel.kt
+++ app/src/main/java/com/danteyu/studio/moodietrail/note/NoteViewModel.kt
@@ -1,7 +1,10 @@
 package com.danteyu.studio.moodietrail.note
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.danteyu.studio.moodietrail.data.source.MoodieTrailRepository
+import com.danteyu.studio.moodietrail.network.LoadApiStatus
 
 
 /**
@@ -9,4 +12,23 @@ import com.danteyu.studio.moodietrail.data.source.MoodieTrailRepository
  *
  * The [ViewModel] that is attached to the [NoteFragment].
  */
-class NoteViewModel(private val moodieTrailRepository: MoodieTrailRepository) : ViewModel() {}
+class NoteViewModel(private val moodieTrailRepository: MoodieTrailRepository) : ViewModel() {
+
+    // status: The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<LoadApiStatus>()
+
+    val status: LiveData<LoadApiStatus>
+        get() = _status
+
+    // error: The internal MutableLiveData that stores the error of the most recent request
+    private val _error = MutableLiveData<String>()
+
+    val error: LiveData<String>
+        get() = _error
+
+    // status for the loading icon of swl
+    private val _refreshStatus = MutableLiveData<Boolean>()
+
+    val refreshStatus: LiveData<Boolean>
+        get() = _refreshStatus
+}
