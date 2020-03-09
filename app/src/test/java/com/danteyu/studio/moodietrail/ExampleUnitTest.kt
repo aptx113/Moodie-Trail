f02c90dcc8f17c54a5d5a0af5e01cc4877e9217e
aptx113 <aptx113@gmail.com>
2020-03-09 13:42:25 +0800

    Perform InstrumentedTest to test time related function

--- app/src/test/java/com/danteyu/studio/moodietrail/ExampleUnitTest.kt
+++ app/src/test/java/com/danteyu/studio/moodietrail/ExampleUnitTest.kt
@@ -1,5 +1,9 @@
 package com.danteyu.studio.moodietrail
 
+import com.danteyu.studio.moodietrail.data.AverageMood
+import com.danteyu.studio.moodietrail.data.Note
+import com.danteyu.studio.moodietrail.util.Util.getEndTimeOfDay
+
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +17,33 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun averageMoodScore_isCorrect() {
+
+        val notes = mutableListOf<Note>().apply {
+            add(Note(mood = 2))
+            add(Note(mood = 3))
+            add(Note(mood = 4))
+            add(Note(mood = 2))
+        }
+        var totalMoodScore = 0f
+        var avgMoodScore = 0f
+
+        if (notes.count() > 0) {
+            notes.forEach { note ->
+                note.mood.let {
+                    totalMoodScore += note.mood
+                }
+            }
+
+            avgMoodScore = totalMoodScore / notes.count()
+        }
+
+        avgMoodScore
+
+        assertEquals((2 + 3 + 4 + 2) / notes.count().toFloat(), avgMoodScore)
+
     }
 }
